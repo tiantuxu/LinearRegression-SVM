c5ab19cf70771adcfa3728b3be3d3b98d873ee22
Tiantu Xu <xu944@purdue.edu>
2019-03-07 11:14:43 -0500

    up

--- bonus_lr_svm_fine_tune.py
+++ bonus_lr_svm_fine_tune.py
@@ -12,9 +12,8 @@ import numpy as np
 # In[2]:
 
 
-trainingDataFilename = sys.argv[1]
-testDataFilename = sys.argv[2]
-modelIdx = int(sys.argv[3])
+trainingDataFilename = 'trainingSet.csv'
+testDataFilename = 'testSet.csv'
 
 
 # In[3]:
@@ -29,21 +28,20 @@ def sigmoid(scores):
             predictions[i] += np.exp(scores[i]) / (1.0 + np.exp(scores[i]))
     return predictions
 
-def lr(trainingSet, testSet):
-    #print len(trainingSet.columns)
-    regularization = 0.01
-    step_size = 0.01
-    
-    max_iterations = 500
+def lr(trainingSet, testSet, regularization, step_size):
+    max_iterations = 1000
     tol = 1e-6
     
     count = 0
     
-    train_labels = trainingSet['decision']    
-    trainingSet = trainingSet.drop('decision', axis=1)
+    cvSet = trainingSet.tail(1300)
+    train = trainingSet.head(3900)
+    train_labels = train['decision']
+    trainingSet = train.drop('decision', axis=1)
     
     #print train_labels, trainingSet
-    w = np.zeros(len(trainingSet.columns) + 1)
+    #w = np.zeros(len(trainingSet.columns) + 1)
+    w = np.random.normal(0, 1, len(trainingSet.columns) + 1)
     
     # Add intercept
     X = np.array(trainingSet)
@@ -71,27 +69,30 @@ def lr(trainingSet, testSet):
         
         diff = abs(norm_new - norm_old)
         #print w
-        #print count, diff
+    cv_accuracy = get_accuracy_lr(w, trainingSet, cvSet)
     
-    return w
+    return w, cv_accuracy
 
 
 # In[4]:
 
 
-def svm(trainingSet, testSet):
+def svm(trainingSet, testSet, regularization, step_size):
     #print len(trainingSet.columns)
-    regularization = 0.01
-    step_size = 0.50
+#     regularization = 0.01
+#     step_size = 0.50
     
-    max_iterations = 500
+    max_iterations = 1000
     tol = 1e-6
     #print len(trainingSet[trainingSet['decision'] == 1])
     count = 0
-    train_labels = trainingSet['decision']    
-    trainingSet = trainingSet.drop('decision', axis=1)
+    cvSet = trainingSet.tail(1300)
+    train = trainingSet.head(3900)
+    train_labels = train['decision']
+    trainingSet = train.drop('decision', axis=1)
 
-    w = np.zeros(len(trainingSet.columns) + 1)
+    #w = np.zeros(len(trainingSet.columns) + 1)
+    w = np.random.normal(0, 1, len(trainingSet.columns) + 1)
     
     # Add intercept
     X = np.array(trainingSet)
@@ -110,15 +111,7 @@ def svm(trainingSet, testSet):
         count += 1
         norm_old = np.linalg.norm(w)
         
-        predictions = np.dot(X, w)
-        #print X.shape, w.shape,predictions.shape
-#         p = []
-#         for i in range(0, 5200):
-#             p.append(sum([w[j] * X[i][j] for j in range(len(trainingSet.columns))]))
-        
-#         print predictions, p 
-        #print len(predictions)
-        #print np.dot(w, X[0]), predictions[0]     
+        predictions = np.dot(X, w)    
         error = 0
         gradient = np.zeros(len(w))
         for i in range(len(predictions)):
@@ -138,45 +131,20 @@ def svm(trainingSet, testSet):
         diff = abs(norm_new - norm_old)
         #print count, diff, error
     #print w
-    return w
+    cv_accuracy = get_accuracy_lr(w, trainingSet, cvSet)
+    
+    return w, cv_accuracy
 
 
 # In[5]:
 
 
 def get_accuracy_lr(w, trainingSet, testSet):
-    total_train = len(trainingSet)
-    count_train = 0
     total_test = len(testSet)
     count_test = 0
     
-    train_labels = trainingSet['decision']
     test_labels = testSet['decision']
-    
-    trainingSet = trainingSet.drop('decision', axis=1)
     testSet = testSet.drop('decision', axis=1)
-    
-    # Training accuracy
-    X = np.array(trainingSet)
-    Y = np.array(train_labels)
-    intercept = np.ones((X.shape[0], 1))
-    X = np.hstack((X, intercept))
-    
-    scores = np.dot(X, w)
-    predictions = sigmoid(scores)
-        
-    for i in range(len(predictions)):
-        if predictions[i] > 0.5:
-            predictions[i] = 1
-        else:
-            predictions[i] = 0
-
-    for i in range(len(predictions)):    
-        if predictions[i] == int(Y[i]):
-            count_train += 1
-
-    training_accuracy = 1.0 * count_train/total_train
-    print 'Training Accuracy LR:', '%.2f' % training_accuracy
     
     # Test accuracy
     X = np.array(testSet)
@@ -186,7 +154,6 @@ def get_accuracy_lr(w, trainingSet, testSet):
 
     scores = np.dot(X, w)
     predictions = sigmoid(scores)
-
     for i in range(len(predictions)):
         if predictions[i] > 0.5:
             predictions[i] = 1
@@ -198,40 +165,15 @@ def get_accuracy_lr(w, trainingSet, testSet):
             count_test += 1
             
     test_accuracy = 1.0 * count_test/total_test
-    print 'Test Accuracy LR:', '%.2f' % test_accuracy
+    #print 'Test Accuracy LR:', '%.2f' % test_accuracy
+    return test_accuracy
     
 def get_accuracy_svm(w, trainingSet, testSet):
-    total_train = len(trainingSet)
-    count_train = 0
     total_test = len(testSet)
     count_test = 0
     
-    train_labels = trainingSet['decision']
     test_labels = testSet['decision']
-    
-    trainingSet = trainingSet.drop('decision', axis=1)
     testSet = testSet.drop('decision', axis=1)
-    
-    # Training accuracy
-    X = np.array(trainingSet)
-    Y = np.array(train_labels)
-    intercept = np.ones((X.shape[0], 1))
-    X = np.hstack((intercept, X))
-    
-    predictions = np.dot(X, w)
-    
-    for i in range(len(predictions)):
-        if predictions[i] > 0.0:
-            predictions[i] = 1
-        else:
-            predictions[i] = 0
-
-    for i in range(len(predictions)):    
-        if predictions[i] == int(Y[i]):
-            count_train += 1
-
-    training_accuracy = 1.0 * count_train/total_train
-    print 'Training Accuracy SVM:', '%.2f' % training_accuracy
     
     # Test accuracy
     X = np.array(testSet)
@@ -252,7 +194,8 @@ def get_accuracy_svm(w, trainingSet, testSet):
             count_test += 1
             
     test_accuracy = 1.0 * count_test/total_test
-    print 'Test Accuracy SVM:', '%.2f' % test_accuracy
+    #print 'Test Accuracy SVM:', '%.2f' % test_accuracy
+    return test_accuracy
 
 
 # In[6]:
@@ -261,12 +204,36 @@ def get_accuracy_svm(w, trainingSet, testSet):
 trainingSet = pd.read_csv(trainingDataFilename)
 testSet = pd.read_csv(testDataFilename)
 
-if modelIdx == 1:
-    w = lr(trainingSet, testSet)
-    get_accuracy_lr(w, trainingSet, testSet)
-else:
-    w = svm(trainingSet, testSet)
-    get_accuracy_svm(w, trainingSet, testSet)
+regularization = [0.01, 0.005, 0.002, 0.001, 0.0005, 0.0002, 0.0001]
+step_size = [0.01, 0.005, 0.002, 0.001, 0.0005, 0.0002, 0.0001]
+
+print 'Fine tuning regularization and step_size...'
+
+max_accuracy = 0.0
+for r in regularization:
+    for s in step_size:
+        #print 'regularization =', r, 'step_size =', s
+        w, a = lr(trainingSet, testSet, r, s)
+        if a > max_accuracy:
+            max_accuracy = a
+            r_max = r
+            s_max = s
+            w_max = w
+        
+get_accuracy_lr(w_max, trainingSet, testSet)
+print 'Have max test accuracy:', '%.2f' % max_accuracy, 'when regularization =', r_max, 'and step_size =', s
+max_accuracy = 0.0
+for r in regularization:
+    for s in step_size:
+        #print 'regularization =', r, 'step_size =', s
+        w, a = svm(trainingSet, testSet, r, s)
+        if a > max_accuracy:
+            max_accuracy = a
+            r_max = r
+            s_max = s
+            w_max = w
+get_accuracy_svm(w_max, trainingSet, testSet)
+print 'Have max test accuracy:', '%.2f' % max_accuracy, 'when regularization =', r_max, 'and step_size =', s
 
 
 # In[ ]:
